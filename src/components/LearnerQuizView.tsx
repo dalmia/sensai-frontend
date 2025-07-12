f1ba2f928b665637d88af96e71a661bed6676acf
Aman Dalmia <amandalmia18@gmail.com>
2025-07-12 11:33:00 +0530

    fix more mistakes

--- src/components/LearnerQuizView.tsx
+++ src/components/LearnerQuizView.tsx
@@ -1204,6 +1204,7 @@ export default function LearnerQuizView({
                 });
         },
         [
+            user,
             validQuestions,
             currentQuestionIndex,
             onSubmitAnswer,
