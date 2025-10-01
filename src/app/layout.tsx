2e9e0badc5fa83c2cc551d11ad32b7ba64d2c7c6
dalmia <amandalmia18@gmail.com>
2025-10-01 15:18:36 +0530

    optimise docker build time

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,14 @@ const geistSans = Geist({
   display: "swap",
   weight: ["400", "500", "600"],
   fallback: ["system-ui", "arial"],
+  fallback: ["system-ui", "arial"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
   display: "swap",
-  fallback: ["system-ui", "arial"],
+  fallback: ["ui-monospace", "monospace"],
 });
 
 export const metadata: Metadata = {
